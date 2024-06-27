dc08bd02efbd1b84ed9a32d4c2200c7b1dcd6ca1
s1F102402181 <s1F102402181@iniad.org>
2024-06-27 16:16:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
+kinoko_count = 3
+takenoko_count = 5
+messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
 
 kinoko_count = 3
 takenoko_count = 5
@@ -12,6 +15,7 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+<<<<<<< Updated upstream
     global kinoko_count, takenoko_count, messages
     if request.form.get("item") == 'kinoko':
         kinoko_count += 1
@@ -36,6 +40,10 @@ def answer():
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
             message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
 
+=======
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+>>>>>>> Stashed changes
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
