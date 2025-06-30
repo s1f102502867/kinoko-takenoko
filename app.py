bfd99f11f618af2becc47dc72f5ad2043414b111
s1f102502867 <s1f102502867@iniad.org>
2025-06-30 15:46:22 +0900

    change app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko_count = 3
+takenoko_count = 5
+message = ['Kinoko is wounderful!', 'Takenoko is awesome!']
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count)
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count)
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
